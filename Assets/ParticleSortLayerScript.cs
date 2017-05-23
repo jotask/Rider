49c7b9efde8a9db1b20001190a0f69390ac03414
jota_sk <jota_sk@hotmail.com>
2017-05-23 15:17:59 +0100

    player with head

---
+++ Assets/ParticleSortLayerScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(ParticleSystem))]
+public class ParticleSortLayerScript : MonoBehaviour {
+	
+	void Awake () {
+		GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingLayerName = "Background";	
+	}
+
+}
---
+++ Assets/player/Head.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : MonoBehaviour {
+
+	public GameObject prefab;
+
+	void Awake () {
+		GameObject obj = Instantiate (prefab);
+		obj.transform.localScale = this.transform.localScale;
+
+		HingeJoint2D hinge = obj.GetComponent<HingeJoint2D> ();
+		hinge.connectedBody = GetComponent<Rigidbody2D> ();
+		hinge.anchor = new Vector2 (-0f, -2f);
+		hinge.connectedAnchor = new Vector2 (-0.9f, 1f);
+		JointAngleLimits2D limits = new JointAngleLimits2D();
+		limits.min = -15;
+		limits.max = 15;
+		hinge.limits = limits;
+
+	}
+
+
+}
