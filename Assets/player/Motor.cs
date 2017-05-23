e928050c9701664efc8681f4fb1fa395278fc7cf
jota_sk <jota_sk@hotmail.com>
2017-05-23 13:56:59 +0100

    player controller

--- Assets/player/Motor.cs
+++ Assets/player/Motor.cs
@@ -2,41 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Rigidbody2D))]
 public class Motor : MonoBehaviour {
 
 	//public float suspension;
 
 	public float speed = 1500;
-	public float movement = 1;
+	public float rotationSpeed = 15f;
+
+	private float movement = 0f;
+	private float rotation = 0f;
 
 	public WheelJoint2D back;
 	public WheelJoint2D front;
 
+	private Rigidbody2D body;
+
 	void Awake(){
-		
+		this.body = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update () {
-		this.movement = -Input.GetAxisRaw ("Horizontal") * speed;
+		this.movement = -Input.GetAxisRaw ("Vertical") * this.speed;
+		this.rotation = Input.GetAxisRaw ("Horizontal") * this.rotationSpeed;
 	}
 
 	void FixedUpdate(){
 
 		if (movement == 0) {
-			back.useMotor = false;
-			front.useMotor = false;
+			this.back.useMotor = false;
+			this.front.useMotor = false;
 		} else {
-			back.useMotor = true;
-			front.useMotor = true;
+			this.back.useMotor = true;
+			this.front.useMotor = true;
 
 			JointMotor2D motor = new JointMotor2D{
-				motorSpeed = movement,
-				maxMotorTorque = back.motor.maxMotorTorque
+				motorSpeed = this.movement,
+				maxMotorTorque = this.back.motor.maxMotorTorque
 			};
 
-			back.motor = motor;
-			front.motor = motor;
+			this.back.motor = motor;
+			this.front.motor = motor;
 		}
+
+		this.body.AddTorque (-rotation * Time.fixedDeltaTime);
 
 	}
 
