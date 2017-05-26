89f1e8ec245fb7c9530dfe4d2672e98802de4978
jota_sk <jota_sk@hotmail.com>
2017-05-26 20:25:10 +0100

    invicibility and improments

--- Assets/player/HeadCollider.cs
+++ Assets/player/HeadCollider.cs
@@ -5,14 +5,17 @@ public class HeadCollider : MonoBehaviour
 
 	private Hud hud;
 
+	private Player player;
+
 	void Start ()
 	{
 		hud = GameObject.FindGameObjectWithTag("Ui").GetComponent<Hud>();
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.gameObject.tag.ToLower() == "world")
+		if (other.gameObject.tag.ToLower() == "world" && !player.invecible)
 		{
 			hud.GameOver();
 		}
